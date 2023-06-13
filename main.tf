81793cffa732506666766269d1164b3bccdfe1c2
qooqlecom <126831826+qooqlecom@users.noreply.github.com>
2023-06-13 18:19:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ region = var.region
 
 
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
-instance_type = "t2.micro"
-availability_zone = "us-east-2a"
-key_name = "my-laptop-key"
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_pair
 }
